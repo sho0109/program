d7ad37d3a31c7f7c4637463d27c7901ca8964427
sho0109 <133620798+sho0109@users.noreply.githab.com>
2023-06-10 13:39:48 +0900

    フォルダの移動

--- PHP/rensyu.php
+++ PHP/rensyu.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="jp">
-<head>
+<head><?php
+ini_set('display_errors', 1);
+?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
